3cb776ff3a69391dff9079cdcc57377b4413d668
J3cks <jecks.pursuitscoding@gmail.com>
2022-09-25 17:50:33 +0800

    Added round and score tally

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-console.log("Go Jecks!");
-console.log("Para sa Pangarap!");
+console.log("Go J3cks!");
+console.log("I will become a developer!");
 
 function playerChoice() {
     let userChoice = prompt("Pick one: (rock paper scissor)");
@@ -12,6 +12,9 @@ function getComputerChoice() {
     compChoice = choices[Math.floor(Math.random()*choices.length)];
     return compChoice;
 }
+
+let scoreHuman = 0;
+let scorePc = 0;
 
 function playRound(playerSelect, computerSelect) {
     playerSelect = playerChoice();
@@ -35,23 +38,46 @@ function playRound(playerSelect, computerSelect) {
 
     if (evaluatRound() === rockWins) {
         console.log("You win! rock beats scissor");
+        scoreHuman += 1;
     } else if (evaluatRound() === rockTies) {
         console.log("It's a tie! rocks ties with rock!"); 
     }
       else if (evaluatRound() === rockLose) {
         console.log("You lose! paper beats rock");
+        scorePc += 1;
     }
       else if (evaluatRound() === paperWins) {
         console.log("You win! paper beats rock");
+        scoreHuman += 1;
     } else if (evaluatRound() === paperTies) {
         console.log("It's a tie! paper ties with paper");
     } else if (evaluatRound() === paperLose) {
         console.log("You lose! scissor beats paper");
+        scorePc += 1;
     } else if (evaluatRound() === scissorWins) {
         console.log("You win! scissor beats paper");
+        scoreHuman += 1;
     } else if (evaluatRound() === scissorTies) {
         console.log("It's a tie! scissor ties with scissor");
     } else if (evaluatRound() === scissorLose) {
         console.log("You lose! scissor loses to rock");
+        scorePc += 1;
+    }
+
+    console.log(scoreHuman, scorePc);
+}
+
+function game() {
+    for (let i = 1; i < 7; i++) {
+        console.log("Round", i)
+        playRound();
+        if (scoreHuman == 3) {
+            console.log("You win!")
+            break;
+        } else if (scorePc == 3) {
+            console.log("You Lose!")
+            break;
+        }
     }
 }
+game();
