0ee7d16d1614df70b78b21e3dd8886f5a89656ee
J3cks <jecks.pursuitscoding@gmail.com>
2023-08-22 22:14:51 +0800

    added content, for further ui editing

--- script.js
+++ script.js
@@ -1,83 +1,83 @@
 console.log("Go J3cks!");
 console.log("I will become a developer!");
-
-function playerChoice() {
-    let userChoice = prompt("Pick one: (rock paper scissor)");
-    userChoice = userChoice.toLowerCase();
-    return userChoice;
-}
-
-function getComputerChoice() {
-    let choices = ["rock", "paper", "scissor"];
-    compChoice = choices[Math.floor(Math.random()*choices.length)];
-    return compChoice;
-}
-
-let scoreHuman = 0;
-let scorePc = 0;
-
-function playRound(playerSelect, computerSelect) {
-    playerSelect = playerChoice();
-    computerSelect = getComputerChoice();
-    let evaluateRound = (playerSelect, computerSelect);
-    console.log(playerSelect, computerSelect);
-    let evaluatRound = function(roundS) {
-      roundS = (playerSelect + computerSelect) 
-      return roundS;
-    };
-
-    const rockWins = ("rockscissor");
-    const rockTies = ("rockrock" );
-    const rockLose = ("rockpaper");
-    const paperWins = ("paperrock");
-    const paperTies = ("paperpaper");
-    const paperLose = ("paperscissor");
-    const scissorWins = ("scissorpaper");
-    const scissorTies = ("scissorscissor");
-    const scissorLose = ("scissorrock");
-
-    if (evaluatRound() === rockWins) {
-        console.log("You win! rock beats scissor");
-        scoreHuman += 1;
-    } else if (evaluatRound() === rockTies) {
-        console.log("It's a tie! rocks ties with rock!"); 
-    }
-      else if (evaluatRound() === rockLose) {
-        console.log("You lose! paper beats rock");
-        scorePc += 1;
-    }
-      else if (evaluatRound() === paperWins) {
-        console.log("You win! paper beats rock");
-        scoreHuman += 1;
-    } else if (evaluatRound() === paperTies) {
-        console.log("It's a tie! paper ties with paper");
-    } else if (evaluatRound() === paperLose) {
-        console.log("You lose! scissor beats paper");
-        scorePc += 1;
-    } else if (evaluatRound() === scissorWins) {
-        console.log("You win! scissor beats paper");
-        scoreHuman += 1;
-    } else if (evaluatRound() === scissorTies) {
-        console.log("It's a tie! scissor ties with scissor");
-    } else if (evaluatRound() === scissorLose) {
-        console.log("You lose! scissor loses to rock");
-        scorePc += 1;
-    }
-
-    console.log(scoreHuman, scorePc);
-}
-
-function game() {
-    for (let i = 1; i < 7; i++) {
-        console.log("Round", i)
-        playRound();
-        if (scoreHuman == 3) {
-            console.log("You win!")
-            break;
-        } else if (scorePc == 3) {
-            console.log("You Lose!")
-            break;
-        }
-    }
-}
-game();
+// 
+// function playerChoice() {
+    // let userChoice = prompt("Pick one: (rock paper scissor)");
+    // userChoice = userChoice.toLowerCase();
+    // return userChoice;
+// }
+// 
+// function getComputerChoice() {
+    // let choices = ["rock", "paper", "scissor"];
+    // compChoice = choices[Math.floor(Math.random()*choices.length)];
+    // return compChoice;
+// }
+// 
+// let scoreHuman = 0;
+// let scorePc = 0;
+// 
+// function playRound(playerSelect, computerSelect) {
+    // playerSelect = playerChoice();
+    // computerSelect = getComputerChoice();
+    // let evaluateRound = (playerSelect, computerSelect);
+    // console.log(playerSelect, computerSelect);
+    // let evaluatRound = function(roundS) {
+    //   roundS = (playerSelect + computerSelect) 
+    //   return roundS;
+    // };
+// 
+    // const rockWins = ("rockscissor");
+    // const rockTies = ("rockrock" );
+    // const rockLose = ("rockpaper");
+    // const paperWins = ("paperrock");
+    // const paperTies = ("paperpaper");
+    // const paperLose = ("paperscissor");
+    // const scissorWins = ("scissorpaper");
+    // const scissorTies = ("scissorscissor");
+    // const scissorLose = ("scissorrock");
+// 
+    // if (evaluatRound() === rockWins) {
+        // console.log("You win! rock beats scissor");
+        // scoreHuman += 1;
+    // } else if (evaluatRound() === rockTies) {
+        // console.log("It's a tie! rocks ties with rock!"); 
+    // }
+    //   else if (evaluatRound() === rockLose) {
+        // console.log("You lose! paper beats rock");
+        // scorePc += 1;
+    // }
+    //   else if (evaluatRound() === paperWins) {
+        // console.log("You win! paper beats rock");
+        // scoreHuman += 1;
+    // } else if (evaluatRound() === paperTies) {
+        // console.log("It's a tie! paper ties with paper");
+    // } else if (evaluatRound() === paperLose) {
+        // console.log("You lose! scissor beats paper");
+        // scorePc += 1;
+    // } else if (evaluatRound() === scissorWins) {
+        // console.log("You win! scissor beats paper");
+        // scoreHuman += 1;
+    // } else if (evaluatRound() === scissorTies) {
+        // console.log("It's a tie! scissor ties with scissor");
+    // } else if (evaluatRound() === scissorLose) {
+        // console.log("You lose! scissor loses to rock");
+        // scorePc += 1;
+    // }
+// 
+    // console.log(scoreHuman, scorePc);
+// }
+// 
+// function game() {
+    // for (let i = 1; i < 7; i++) {
+        // console.log("Round", i)
+        // playRound();
+        // if (scoreHuman == 3) {
+            // console.log("You win!")
+            // break;
+        // } else if (scorePc == 3) {
+            // console.log("You Lose!")
+            // break;
+        // }
+    // }
+// }
+// game();
